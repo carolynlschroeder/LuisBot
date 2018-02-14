f447d065d175a25bf39500736dd2ade33a3d2eb8
Carolyn <Schroeder>
2018-02-14 10:10:13 -0600

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=carolynluis-Web-Deploy.pubxml -p:Password=soleYoobeegh2cdcAoHoMuiwGn3JSAL6B19FeRvqcQPzngzYfzph60C1atwn
+
